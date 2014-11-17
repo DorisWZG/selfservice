064f4c7194acc112687f9a00ca9eb4b52cf2cc02
xiao <zghanxiao@gmail.com>
2014-11-17 15:31:29 -0800

    create a mysql database with a django super user

--- selfservice/settings.py
+++ selfservice/settings.py
@@ -64,8 +64,10 @@ WSGI_APPLICATION = 'selfservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'self_service',
+	'USER': 'self_service',
+	'PASSWORD': 'cmbjxccwtn',
     }
 }
 
