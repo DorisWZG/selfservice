9c0c170e2489a11a0b2a5f63e819e2afb5aecb19
mglogou <hongyue.chi@gmail.com>
2014-12-09 13:51:35 -0800

    js and html modified

--- static/javascript/testGetResult.js
+++ static/javascript/testGetResult.js
@@ -1,16 +1,34 @@
-function showResult() {
+function check_submit() {
     var category = $("#selectCategory").val();
     var budget = $("#selectBudget").val();
-    var url = "/budget_allocation/metrics_list/industry/" + category + "/budget/" + budget;
+     if (budget != "" && category != "") {
+        $('input[type="submit"]').removeClass('disabled');
+     }
+     else $('input[type="submit"]').addClass();
+//    if ($(this).val().length == 0) {
+//        $(":submit").attr("disabled", true);
+//    } else {
+//        $(":submit").removeAttr("disabled");
+//    }
+}
+function showResult() {
+    $("#submit").attr('disabled','disabled');
+    var category = $("#selectCategory").val();
+
+    var budget = $("#selectBudget").val();
+    if (budget != "" && category != "") {
+        $('input[type="submit"]').removeAttr('disabled');
+    }
+    var url = "/budget_allocation/metrics_result/industry/" + category + "/budget/" + budget;
     $.get(url).done(function(data) {
         var table = $("#result-table");
         table.find("tr").slice(1).remove();
         table.addClass("hidden");
         $(data).each(function(idx, value) {
             var row = $("<tr></tr>");
-            $("<td></td>").html(value["channelID"]).appendTo(row);
+            $("<td></td>").html(value["channelName"]).appendTo(row);
             $("<td></td>").html(value["allocation"]).appendTo(row);
-            $("<td></td>").html(value["budget"]).appendTo(row);
+            $("<td></td>").html(value["expectedClicks"]).appendTo(row);
             $("<td></td>").html(value["costPerClick"]).appendTo(row);
             $("<td></td>").html(value["expectedImpressions"]).appendTo(row);
             $("<td></td>").html(value["costPerImpression"]).appendTo(row);
@@ -18,7 +36,7 @@ function showResult() {
         });
         table.removeClass("hidden");
     }).fail(function(data) {
-        alert(data);
+        alert("Status Code: " + data.status + ", Status Text: " + data.statusText + ", Response Text: " + data.responseText);
     });
 
 }
