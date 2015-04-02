53002c765044fccd3720bf6d048e33329ea2e708
mglogou <hongyue.chi@gmail.com>
2015-04-02 11:18:32 -0700

    .gitignore

--- import_db.py
+++ import_db.py
@@ -3,4 +3,27 @@ __author__ = 'Yuer'
 import xlrd
 import MySQLdb
 
-category = xlrd.open_workbook("")
+category = xlrd.open_workbook("data_import.xls")
+sheet = category.sheet_by_name("industry")
+
+# establish a mysql connection
+database = MySQLdb.connect (host = "localhost", user = "root", passwd = "", db = "self_service")
+
+# get the cursor, which is used to traverse the database, line by line
+cursor = database.cursor()
+
+query = """INSERT INTO budget_allocation_industry (industryId,industryName,subIndustry) VALUES(%s, %s, %s)"""
+
+for i in range(1, sheet.nrows):
+    industryId = sheet.cell(i,0).value
+    industryName = sheet.cell(i, 1).value
+    subIndustry = sheet.cell(i, 2).value
+
+    # assign values from each row
+    values = (industryId, industryName, subIndustry)
+
+    cursor.execute(query, values)
+
+cursor.close()
+database.commit()
+database.close()
