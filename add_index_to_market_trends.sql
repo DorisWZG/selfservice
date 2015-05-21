0dd829c27c5e2f3de6110796b6977766618045c1
lixinglogou <lixinsui@gmail.com>
2015-05-21 13:57:26 -0700

    add index to markettrend table

---
+++ add_index_to_market_trends.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `self_service`.`market_trend` 
+ADD INDEX `index_date_column` (`index_date` DESC),
+ADD INDEX `index_cat1` (`cat1_id` ASC),
+ADD INDEX `index_cat2` (`cat2_id` ASC),
+ADD INDEX `index_cat3` (`cat3_id` ASC);
