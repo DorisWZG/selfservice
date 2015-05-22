2913851667d007d15c8ce89e2e1e198072783b4f
binlee <bin.lee@gmail.com>
2015-05-22 10:51:57 -0700

    Add functions to implement: 1. Normalize the market trend data. 2. Detect sales opportunity function. 3. Once an opportunity is detected, simple message is display on webpage.
    At this moment, the code still has issue if no data at the begining of
    the time period.
    For example, the code does not work well for the following category
    selection.
    member_service/sales_opportunities/?cat1=7&cat2=1032228&cat3=122374007
    
    I had to add code to use middle year data to do normalization,
    somehow, it does not work.
    
    Changes to be committed:
    	modified:   member_service/views.py
    	modified:   templates/member_service/sales_opportunities.html

--- member_service/views.py
+++ member_service/views.py
@@ -18,7 +18,7 @@ import random
 import re
 
 # a global variable to control if URL will show category ID or category name
-# __IMPORTANT_NOTE__, this setting needs to be consistent with sales_opportunities.js
+# __IMPORTANT_NOTE__, this setting needs to be consistent with setting in sales_opportunities.js
 g_URL_SHOW_CAT_ID_OR_NAME = 0       # 0, show category ID, 1, show category name
 
 # function ValidPhone(...) , ValidEmail(...) and subscriber_contact(...) related to subscriber newsletter
@@ -96,6 +96,8 @@ def getCat3(request, cat2):
         result.append({ 'id': str(cat3.cat3_id), 'eng_kw': cat3.eng_kw })
     return HttpResponse(json.dumps(result), content_type='application/json')
 
+# The algorithm to normlize data:
+# 
 def sales_opportunities(request):
     context = request.GET.dict()
 
@@ -115,17 +117,153 @@ def sales_opportunities(request):
 
         purchase_index1688, purchase_indexTb, supply_index = [], [], []
 
+        # get average for initial period, we will use this average to normalize the dataset.
+        purchase_index1688_av, purchase_indexTb_av, supply_index_av  = market_trend_get_average_over_period(result, 0, 14)
+
+        #  If the average for a dataset is NOT zero, we will divide the average to normlize the data.
+        # Sometime, there are no data for one index. The average is ZEO, we can not do division.
+        # Let's use another time frame to try to get another average
+
+        # __BL_COMMENT__, somehow, if we add this attempt to get average, it did not work.
+        # Do not know why, will take a look later.
+        '''
+        if (supply_index_av == 0):
+            # Use the middle year data to get average again
+            purchase_index1688_av, purchase_indexTb_av, supply_index_av  = market_trend_get_average_over_period(result, 180,180+14)
+        '''
+
+        # After second try, if average is still zero, then, we do the following trick
+        # To make calculation simple, we just set average to 1, since 0/1 is still 0.
+        # this make future calculation simpler.
+        # For future improvement, if the average for initial period is ZERO, we shall try to
+        # find another time period to do average. __BL_COMMENT__
+        if (purchase_index1688_av == 0):
+            purchase_index1688_av = 1
+        if (purchase_indexTb_av == 0):
+            purchase_indexTb_av = 1
+        if (supply_index_av == 0):
+            supply_index_av = 1
+
         for record in result:
             epoch = float(time.mktime(record.index_date.timetuple())) * 1000
-            purchase_index1688.append([ epoch, int(record.purchase_index1688) ])
-            purchase_indexTb.append([ epoch, int(record.purchase_indextb) ])
-            supply_index.append([ epoch, int(record.supply_index) ])
+
+            # The data is normlized  by 1000*(data)/average_over_selected_period
+            # where average_over_selected_period at this moment is: purchase_index1688_av, purchase_indexTb_av, supply_index_av
+            purchase_index1688.append([ epoch, int(1000*record.purchase_index1688/purchase_index1688_av) ])
+            purchase_indexTb.append([ epoch, int(1000*record.purchase_indextb/purchase_indexTb_av) ])
+            supply_index.append([ epoch, int(1000*record.supply_index/supply_index_av) ])
+
+        opportunity = Detect_Sales_Opportunities(result)
+
+        # The following text message shall be move to template in sales_opportunities.html
+        if(opportunity > 0):
+            opportunity_msg = 'Great News: There is opportunity for this product in Asia Now. Demand is strong !'
+        else:
+            opportunity_msg = 'No obvious New market opportunity for this product in Asia now. Please subscribe our alert opportunity letter for future opportunity.'
 
         context['purchase_index1688'] = purchase_index1688
         context['purchase_indexTb'] = purchase_indexTb
         context['supply_index'] = supply_index
+        context['opportunity'] = opportunity_msg
 
     return render(request, 'member_service/sales_opportunities.html', context)
+
+# Simple algorithm to detect the potential  opportunities
+# Basic algorithm is to compare the supply and demand data between the end of Q3 and latest data.
+# To get rid of noise and small time fluctuation, we average data over some period.
+# Also, the three index has different scales. To compare them, we need to normalize them.
+#
+# return: -1, data is not available or no opportunity
+#          1, opportunity detected even though there is no supply data
+#          2, opportunity detected when there is supply data
+#
+#  __BL_COMMENT__, maybe in future, we will use different result to show the confidence of our prediction or
+#  how big the opportunity can be.
+def Detect_Sales_Opportunities(result):
+
+    period = 14
+    #starting_date = 0
+    end_Q3_date = 265           # this is the date of 100 earlier. If we add 14 days, is about the end of Q3
+    latest = 350
+
+    # purchase_index1688_av, purchase_indexTb_av, supply_index_av = market_trend_get_average_over_period(result, starting_date, starting_date + period)
+
+    purchase_index1688_av_Q3, purchase_indexTb_av_Q3, supply_index_av_Q3 = market_trend_get_average_over_period(result, end_Q3_date, end_Q3_date + period)
+    purchase_index1688_av_latest, purchase_indexTb_av_latest, supply_index_av_latest = market_trend_get_average_over_period(result, latest, latest + period)
+
+    # Find the difference between Q3 data and latest data
+    demand_change_index1688 = purchase_index1688_av_latest - purchase_index1688_av_Q3
+    demand_change_indexTb = purchase_indexTb_av_latest - purchase_indexTb_av_Q3
+    supply_change = supply_index_av_latest - supply_index_av_Q3
+
+    # Normalize the change according to the value at the beginning of the time period.
+    # After normalization,
+    # demand_change_index1688_normalized, demand_change_indexTb_normalized, supply_change_normalized
+    # all become something like: 0.1, -0.05., or 0.
+    if(purchase_index1688_av_Q3 != 0):
+        demand_change_index1688_normalized = demand_change_index1688/purchase_index1688_av_Q3
+    else:
+        demand_change_index1688_normalized = -1      # use -1 to indicate purchase_index1688_av_Q3 == 0, which means 'data is not available'
+
+    if(purchase_indexTb_av_Q3 != 0):
+        demand_change_indexTb_normalized = demand_change_indexTb/purchase_indexTb_av_Q3
+    else:
+        demand_change_indexTb_normalized = -1      # use -1 to indicate purchase_indexTb_av_Q3 == 0, which means 'data is not available'
+
+    if(supply_index_av_Q3 != 0):
+        supply_change_normalized = supply_change/supply_index_av_Q3
+    else:
+        supply_change_normalized = -1               # use -1 to indicate supply_index_av_Q3 == 0, which means 'data is not available'
+
+    # Algorithm to detect opportunities:
+    opportunity = -1                                # -1 indicate data is not available
+
+    # if we do not know supply data, we have to predict an opportunity only when demand change very big
+    # if we know both demand and supply data, we can use a loose standard
+    Opportunity_threshold_by_demand_data_only = 0.2
+    Opportunity_threshold_by_demand_and_supply_data = 0.1
+
+    # when we do not know supply data
+    if(supply_change_normalized  == -1):
+
+        # make prediction either by retail or wholesale demand change or combined change
+        if (demand_change_index1688_normalized > Opportunity_threshold_by_demand_data_only) or \
+            (demand_change_indexTb_normalized > Opportunity_threshold_by_demand_data_only) or \
+            ((demand_change_index1688_normalized + demand_change_indexTb_normalized) > Opportunity_threshold_by_demand_data_only):
+
+            opportunity = 1
+
+    # when we know supply data
+    else:
+        if (demand_change_index1688_normalized > Opportunity_threshold_by_demand_and_supply_data)  or \
+            (demand_change_indexTb_normalized > Opportunity_threshold_by_demand_and_supply_data) or \
+            ((demand_change_index1688_normalized + demand_change_indexTb_normalized) > Opportunity_threshold_by_demand_and_supply_data):
+
+            opportunity = 2
+
+    return opportunity
+
+#  Get average of market trend data over selected period
+def market_trend_get_average_over_period(result, start, end):
+
+    purchase_index1688_sum = 0
+    purchase_indexTb_sum = 0
+    supply_index_sum = 0
+
+    period = end - start
+
+    for i in range(start, end):
+        record = result[i]
+
+        purchase_index1688_sum += record.purchase_index1688
+        purchase_indexTb_sum += record.purchase_indextb
+        supply_index_sum += record.supply_index
+
+    purchase_index1688_av = purchase_index1688_sum/period
+    purchase_indexTb_av = purchase_indexTb_sum/period
+    supply_index_av = supply_index_sum/period
+
+    return purchase_index1688_av, purchase_indexTb_av, supply_index_av
 
 # create demo data.
 def create_demo_data():
